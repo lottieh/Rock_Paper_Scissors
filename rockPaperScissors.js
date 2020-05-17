84416fe4aa5189b1f57b2fb0c1931cb498a7c6fd
Charlotte House <cdmhouse@sky.com>
2020-05-17 12:32:15 +0100

    implementing node readline

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,33 +1,84 @@
-let player = "cissors"; //Please insert your choice here 
+const readline = require('readline');
 
-console.log("You picked ", player)
+let player = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+player.question('Rock, Paper or Scissors? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log("You picked ", player)
+
+let playerLower = player.toLowerCase();
+console.log(playerLower)
 
 let options = ["rock", "paper", "scissors"];
 computerPick = options[Math.floor(Math.random() * options.length)]
 
 console.log("I picked ", computerPick)
 
-if ((player === ("rock" || "Rock")) && (computerPick === "paper")) {
+if ((playerLower === "rock") && (computerPick === "paper")) {
     response = "Paper beats rock, you lose ! "
 
-} else if ((player === ("rock" || "Rock")) && (computerPick === "scissors")) {
+} else if ((playerLower=== "rock") && (computerPick === "scissors")) {
     response = "Rock beats scissors, you win !"
 
-} else if ((player === ("paper" || "Paper")) && (computerPick === "rock")) {
+} else if ((playerLower === "paper" ) && (computerPick === "rock")) {
     response = "Paper beats rock, you win ! "
 
-} else if ((player === ("paper" || "Paper")) && (computerPick === "scissors")) {
+} else if ((playerLower === "paper" ) && (computerPick === "scissors")) {
     response = "Scissors beat paper, you lose !"
-} else if ((player === ("scissors" || "Scissors")) && (computerPick === "rock")) {
+} else if ((playerLower === "scissors" ) && (computerPick === "rock")) {
     response = "Rock beats scissors, you lose !"
-} else if ((player === ("scissors" || "Scissors")) && (computerPick === "paper")) {
+} else if ((playerLower === "scissors" ) && (computerPick === "paper")) {
     response = "Scissors beat paper, you win !"
-} else if ((player === computerPick)) {
+} else if ((playerLower === computerPick)) {
     response = "We picked the same, it's a tie"
 } else {
     response = "I don't think that is a valid choice!"
 }
 console.log(response)
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  player.close();
+});
+
+
+
+
+// // let player = "Rock"; //Please insert your choice here 
+
+// console.log("You picked ", player)
+
+// let playerLower = player.toLowerCase();
+// console.log(playerLower)
+
+// let options = ["rock", "paper", "scissors"];
+// computerPick = options[Math.floor(Math.random() * options.length)]
+
+// console.log("I picked ", computerPick)
+
+// if ((playerLower === "rock") && (computerPick === "paper")) {
+//     response = "Paper beats rock, you lose ! "
+
+// } else if ((playerLower=== "rock") && (computerPick === "scissors")) {
+//     response = "Rock beats scissors, you win !"
+
+// } else if ((playerLower === "paper" ) && (computerPick === "rock")) {
+//     response = "Paper beats rock, you win ! "
+
+// } else if ((playerLower === "paper" ) && (computerPick === "scissors")) {
+//     response = "Scissors beat paper, you lose !"
+// } else if ((playerLower === "scissors" ) && (computerPick === "rock")) {
+//     response = "Rock beats scissors, you lose !"
+// } else if ((playerLower === "scissors" ) && (computerPick === "paper")) {
+//     response = "Scissors beat paper, you win !"
+// } else if ((playerLower === computerPick)) {
+//     response = "We picked the same, it's a tie"
+// } else {
+//     response = "I don't think that is a valid choice!"
+// }
+// console.log(response)
 
 
 
